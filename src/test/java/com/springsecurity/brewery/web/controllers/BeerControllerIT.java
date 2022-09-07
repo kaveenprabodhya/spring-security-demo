9fe9f5ee362ac1028a4db3a1d4953c4ee6daa902
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-07 22:30:50 +0530

    add tests to beerController and customerController

---
+++ src/test/java/com/springsecurity/brewery/web/controllers/BeerControllerIT.java
@@ -0,0 +1,58 @@
+package com.springsecurity.brewery.web.controllers;
+
+import com.springsecurity.brewery.repositories.BeerInventoryRepository;
+import com.springsecurity.brewery.repositories.BeerRepository;
+import com.springsecurity.brewery.repositories.CustomerRepository;
+import com.springsecurity.brewery.services.BeerService;
+import com.springsecurity.brewery.services.BreweryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+class BeerControllerIT {
+
+    @Autowired
+    WebApplicationContext wac;
+
+    MockMvc mockMvc;
+
+    @MockBean
+    BeerRepository beerRepository;
+
+    @MockBean
+    BeerInventoryRepository beerInventoryRepository;
+
+    @MockBean
+    BreweryService breweryService;
+
+    @MockBean
+    CustomerRepository customerRepository;
+
+    @MockBean
+    BeerService beerService;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(wac)
+                .build();
+    }
+
+    @Test
+    void findBeers() throws Exception {
+        mockMvc.perform(get("/beers/find"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/findBeers"))
+                .andExpect(model().attributeExists("beer"));
+    }
+}
