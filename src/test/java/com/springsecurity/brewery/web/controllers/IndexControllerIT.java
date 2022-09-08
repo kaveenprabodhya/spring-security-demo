0d9003d1dd07e3dbed168142ab1da7eab779d53f
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-08 15:36:40 +0530

    tests get index slash

---
+++ src/test/java/com/springsecurity/brewery/web/controllers/IndexControllerIT.java
@@ -0,0 +1,17 @@
+package com.springsecurity.brewery.web.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+class IndexControllerIT extends BaseIt {
+
+    @Test
+    void testGetIndexSlash() throws Exception{
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+}
