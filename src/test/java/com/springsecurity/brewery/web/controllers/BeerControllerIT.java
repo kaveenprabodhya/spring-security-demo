671b1001d5b15d8eaecc535700b3aafceda4962a
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-08 18:54:00 +0530

    change WebMvcTest to SpringBootTest

--- src/test/java/com/springsecurity/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/com/springsecurity/brewery/web/controllers/BeerControllerIT.java
@@ -20,7 +20,7 @@ import static org.springframework.security.test.web.servlet.setup.SecurityMockMv
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest
+@SpringBootTest
 class BeerControllerIT extends BaseIt {
 
     @WithMockUser("user")
