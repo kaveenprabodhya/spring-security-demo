388271c7606568924cac2d35a874336740ba50af
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-10 23:42:02 +0530

    add stream arguments

--- src/test/java/com/springsecurity/brewery/web/controllers/BaseIt.java
+++ src/test/java/com/springsecurity/brewery/web/controllers/BaseIt.java
@@ -23,4 +23,20 @@ public abstract class BaseIt {
                 .apply(springSecurity())
                 .build();
     }
+
+    public static Stream<Arguments> getStreamAdminCustomer() {
+        return Stream.of(Arguments.of("spring" , "password"),
+                Arguments.of("scott", "tiger"));
+    }
+
+    public static Stream<Arguments> getStreamAllUsers() {
+        return Stream.of(Arguments.of("spring" , "password"),
+                Arguments.of("scott", "tiger"),
+                Arguments.of("user", "password"));
+    }
+
+    public static Stream<Arguments> getStreamNotAdmin() {
+        return Stream.of(Arguments.of("scott", "tiger"),
+                Arguments.of("user", "password"));
+    }
 }
