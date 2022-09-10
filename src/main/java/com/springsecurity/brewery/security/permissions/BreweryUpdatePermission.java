207c311c1a469525990e368e31be5aa91ebf97d7
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-10 23:51:05 +0530

    create custom permissions annotations

---
+++ src/main/java/com/springsecurity/brewery/security/permissions/BreweryUpdatePermission.java
@@ -0,0 +1,8 @@
+package com.springsecurity.brewery.security.permissions;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface BreweryUpdatePermission {
+}
