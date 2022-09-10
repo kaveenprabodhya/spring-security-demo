19d9f605bae0dd722ead2f71701241493e9d3d98
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-10 23:41:36 +0530

    remove mock beans

--- src/test/java/com/springsecurity/brewery/web/controllers/BaseIt.java
+++ src/test/java/com/springsecurity/brewery/web/controllers/BaseIt.java
@@ -1,42 +1,23 @@
 package com.springsecurity.brewery.web.controllers;
 
-import com.springsecurity.brewery.repositories.BeerInventoryRepository;
-import com.springsecurity.brewery.repositories.BeerRepository;
-import com.springsecurity.brewery.repositories.CustomerRepository;
-import com.springsecurity.brewery.services.BeerService;
-import com.springsecurity.brewery.services.BreweryService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.provider.Arguments;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.util.stream.Stream;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 public abstract class BaseIt {
     @Autowired
     WebApplicationContext wac;
-
     protected MockMvc mockMvc;
 
-    @MockBean
-    BeerRepository beerRepository;
-
-    @MockBean
-    BeerInventoryRepository beerInventoryRepository;
-
-    @MockBean
-    BreweryService breweryService;
-
-    @MockBean
-    CustomerRepository customerRepository;
-
-    @MockBean
-    BeerService beerService;
-
     @BeforeEach
-    void setUp() {
+    public void setup() {
         mockMvc = MockMvcBuilders
                 .webAppContextSetup(wac)
                 .apply(springSecurity())
