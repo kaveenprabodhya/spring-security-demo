9ec4357d3b8031ddbf603435041864d0cd64c746
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-11 18:37:30 +0530

    add BeerOrderAuthenticationManager

---
+++ src/main/java/com/springsecurity/brewery/security/BeerOrderAuthenticationManager.java
@@ -0,0 +1,21 @@
+package com.springsecurity.brewery.security;
+
+import com.springsecurity.brewery.domain.security.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class BeerOrderAuthenticationManager {
+
+    public boolean customerIdMatches(Authentication authentication, UUID customerId){
+        User authenticatedUser = (User) authentication.getPrincipal();
+
+        log.debug("Auth user Customer Id: "+authenticatedUser.getCustomer().getId()+" Customer Id: "+customerId);
+
+        return authenticatedUser.getCustomer().getId().equals(customerId);
+    }
+}
