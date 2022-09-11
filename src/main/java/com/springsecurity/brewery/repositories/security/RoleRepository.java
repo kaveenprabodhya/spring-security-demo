683e24fe3f6b8b8c450f1ea7fde5456408079860
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-11 16:16:39 +0530

    add findByName

--- src/main/java/com/springsecurity/brewery/repositories/security/RoleRepository.java
+++ src/main/java/com/springsecurity/brewery/repositories/security/RoleRepository.java
@@ -3,5 +3,8 @@ package com.springsecurity.brewery.repositories.security;
 import com.springsecurity.brewery.domain.security.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository  extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String customer);
 }
