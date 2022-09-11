2db9077bcc42283603c1a672ebd5437836e8d7ae
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-11 18:46:45 +0530

    add listOrders getOrderById

--- src/main/java/com/springsecurity/brewery/services/BeerOrderService.java
+++ src/main/java/com/springsecurity/brewery/services/BeerOrderService.java
@@ -4,6 +4,7 @@ package com.springsecurity.brewery.services;
 
 import com.springsecurity.brewery.web.model.BeerOrderDto;
 import com.springsecurity.brewery.web.model.BeerOrderPagedList;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.UUID;
@@ -16,4 +17,8 @@ public interface BeerOrderService {
     BeerOrderDto getOrderById(UUID customerId, UUID orderId);
 
     void pickupOrder(UUID customerId, UUID orderId);
+
+    BeerOrderPagedList listOrders(Pageable pageable);
+
+    BeerOrderDto getOrderById(UUID orderId);
 }
