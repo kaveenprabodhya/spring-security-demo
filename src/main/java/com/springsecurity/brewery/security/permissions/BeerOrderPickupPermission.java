904bdcc0fd67092c50196f4656a903d1da76dbb4
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-11 18:41:25 +0530

    create beer order permission annotations

---
+++ src/main/java/com/springsecurity/brewery/security/permissions/BeerOrderPickupPermission.java
@@ -0,0 +1,13 @@
+package com.springsecurity.brewery.security.permissions;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasAuthority('order.pickup') OR " +
+        "hasAuthority('customer.order.pickup') " +
+        " AND @beerOrderAuthenticationManger.customerIdMatches(authentication, #customerId )")
+public @interface BeerOrderPickupPermission {
+}
