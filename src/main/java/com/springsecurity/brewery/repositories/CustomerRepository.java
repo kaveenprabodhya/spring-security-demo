a5ede93ac3378b81a70ce4690f10499b9832f8ef
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-11 18:47:54 +0530

    find findAllByCustomerName

--- src/main/java/com/springsecurity/brewery/repositories/CustomerRepository.java
+++ src/main/java/com/springsecurity/brewery/repositories/CustomerRepository.java
@@ -5,9 +5,12 @@ import com.springsecurity.brewery.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
     List<Customer> findAllByCustomerNameLike(String customerName);
+
+    Optional<Customer> findAllByCustomerName(String customerName);
 }
