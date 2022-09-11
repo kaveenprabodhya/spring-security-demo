91253deab07cebb52033806c3764f316479b52ac
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-11 15:01:57 +0530

    update UserDetails impl

--- src/main/java/com/springsecurity/brewery/security/JpaUserDetailsService.java
+++ src/main/java/com/springsecurity/brewery/security/JpaUserDetailsService.java
@@ -1,22 +1,13 @@
 package com.springsecurity.brewery.security;
 
-import com.springsecurity.brewery.domain.security.Authority;
-import com.springsecurity.brewery.domain.security.User;
 import com.springsecurity.brewery.repositories.security.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -28,23 +19,9 @@ public class JpaUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         log.debug("Getting user info via JPA");
-        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username+" not found!"));
-        return new
-                org.springframework.security.core.userdetails.User(user.getUsername(),
-                user.getPassword(),
-                user.getEnabled(),
-                user.getAccountNonExpired(),
-                user.getCredentialsNonExpired(), user.getAccountNonLocked(), convertToSpringAuthorities(user.getAuthorities()));
+        return userRepository
+                .findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username+" not found!"));
     }
 
-    private Collection<? extends GrantedAuthority> convertToSpringAuthorities(Set<Authority> authorities) {
-        if(authorities != null && authorities.size() > 0){
-            return authorities.stream()
-                    .map(Authority::getPermission)
-                    .map(SimpleGrantedAuthority::new)
-                    .collect(Collectors.toList());
-        } else {
-            return new HashSet<>();
-        }
-    }
 }
