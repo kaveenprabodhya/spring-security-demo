31595dd2a87fdeca55d83e2ff8d8b7d5cc30f01c
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-11 21:21:53 +0530

    add SecurityEvaluationContextExtension

--- src/main/java/com/springsecurity/brewery/config/SecurityConfiguration.java
+++ src/main/java/com/springsecurity/brewery/config/SecurityConfiguration.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -27,6 +28,11 @@ import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityEvaluationContextExtension securityEvaluationContextExtension(){
+        return new SecurityEvaluationContextExtension();
+    }
 
     @Autowired
     private ObjectPostProcessor<Object> oop;
