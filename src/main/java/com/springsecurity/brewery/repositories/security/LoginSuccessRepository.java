7f3a1b6d9b8220b8d4de0e40d65a13ee9e3f205b
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-13 02:48:10 +0530

    add findAllByUserAndCreateDateAfter to LoginSuccessRepository repo

--- src/main/java/com/springsecurity/brewery/repositories/security/LoginSuccessRepository.java
+++ src/main/java/com/springsecurity/brewery/repositories/security/LoginSuccessRepository.java
@@ -1,7 +1,13 @@
 package com.springsecurity.brewery.repositories.security;
 
+import com.springsecurity.brewery.domain.security.LoginFailure;
 import com.springsecurity.brewery.domain.security.LoginSuccess;
+import com.springsecurity.brewery.domain.security.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 public interface LoginSuccessRepository extends JpaRepository<LoginSuccess, Integer> {
+    List<LoginFailure> findAllByUserAndCreateDateAfter(User user, Timestamp timestamp);
 }
