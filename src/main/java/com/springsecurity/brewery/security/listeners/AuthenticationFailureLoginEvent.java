636739662e807974c54ee62f96ef319c14189e58
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-13 02:48:38 +0530

    add AuthenticationFailureLoginEvent

---
+++ src/main/java/com/springsecurity/brewery/security/listeners/AuthenticationFailureLoginEvent.java
@@ -0,0 +1,64 @@
+package com.springsecurity.brewery.security.listeners;
+
+import com.springsecurity.brewery.domain.security.LoginFailure;
+import com.springsecurity.brewery.domain.security.User;
+import com.springsecurity.brewery.repositories.security.LoginFailureRepository;
+import com.springsecurity.brewery.repositories.security.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class AuthenticationFailureLoginEvent {
+
+    private final LoginFailureRepository loginFailureRepository;
+    private final UserRepository userRepository;
+
+    @EventListener
+    public void onFailure(AuthenticationFailureBadCredentialsEvent event) {
+        log.debug("Login failure");
+
+        if (event.getSource() instanceof UsernamePasswordAuthenticationToken token) {
+            LoginFailure.LoginFailureBuilder builder = LoginFailure.builder();
+
+            if (token.getPrincipal() instanceof String) {
+                log.debug("Attempted Username: " + token.getPrincipal());
+                builder.username((String) token.getPrincipal());
+                userRepository.findByUsername((String) token.getPrincipal()).ifPresent(builder::user);
+            }
+
+            if (token.getDetails() instanceof WebAuthenticationDetails details) {
+
+                log.debug("Source IP: " + details.getRemoteAddress());
+                builder.sourceIp(details.getRemoteAddress());
+            }
+            LoginFailure failure = loginFailureRepository.save(builder.build());
+            log.debug("Failure Event: " + failure.getId());
+
+            if (failure.getUser() != null) {
+                lockUserAccount(failure.getUser());
+            }
+        }
+    }
+
+    private void lockUserAccount(User user) {
+        List<LoginFailure> failures = loginFailureRepository.findAllByUserAndCreatedDateIsAfter(user,
+                Timestamp.valueOf(LocalDateTime.now().minusDays(1)));
+
+        if(failures.size() > 3){
+            log.debug("Locking User Account... ");
+            user.setAccountNonLocked(false);
+            userRepository.save(user);
+        }
+    }
+}
