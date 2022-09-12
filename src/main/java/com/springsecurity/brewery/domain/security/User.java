34c93d8c7a15c19608e0c780060ca4c005f035b7
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-13 03:38:28 +0530

    add createDate and lastModifiedDate

--- src/main/java/com/springsecurity/brewery/domain/security/User.java
+++ src/main/java/com/springsecurity/brewery/domain/security/User.java
@@ -2,12 +2,15 @@ package com.springsecurity.brewery.domain.security;
 
 import com.springsecurity.brewery.domain.Customer;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.CredentialsContainer;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -78,4 +81,11 @@ public class User implements UserDetails, CredentialsContainer {
     public void eraseCredentials() {
         this.password = null;
     }
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
 }
